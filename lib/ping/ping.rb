ca4784b7a0f10da67251512a55bb785e0c162c23
pingbox <pingbox@pingbox.(none)>
2015-08-26 11:13:03 -0700

    refactored ping.rb, although there wasn't much to be done :) fixed spacing and removed old code.

--- lib/ping/ping.rb
+++ lib/ping/ping.rb
@@ -2,7 +2,6 @@ $pingbox_root = "#{File.dirname(__FILE__)}/../.." unless $pingbox_root
 
 require 'time'
 
-#this will parse the ping data
 class PingParser
   def initialize(ping_data, test_caseid=0)
     @ping_data = ping_data.split("\n")
@@ -11,9 +10,11 @@ class PingParser
     @test_case_id = test_caseid
     parse
   end
+
   def reply_times
     @ping_data
   end
+
   def ping_replys
     @ping_replys
   end
@@ -59,27 +60,21 @@ class PingParser
   end
 
   def min
-    begin
-      rtt.split("=")[1].split("/")[0].scan(/[\d\.]/).join
-    rescue
-      0
-    end
+    rtt.split("=")[1].split("/")[0].scan(/[\d\.]/).join
+  rescue
+    0
   end
 
   def max
-    begin
-      rtt.split("=")[1].split("/")[2].scan(/[\d\.]/).join
-    rescue
-      0
-    end
+    rtt.split("=")[1].split("/")[2].scan(/[\d\.]/).join
+  rescue
+    0
   end
 
   def average
-    begin
-      rtt.split("=")[1].split("/")[1].scan(/[\d\.]/).join
-    rescue
-      0
-    end
+    rtt.split("=")[1].split("/")[1].scan(/[\d\.]/).join
+  rescue
+    0
   end
 
   def rtt 
@@ -93,28 +88,28 @@ class PingParser
   def parse
     @ping_data.grep(/bytes from/).each do |ping|
       @ping_replys << { 
-        :ping_data => ping,
-        :ping_id => ping.split.grep(/icmp_req/).join.scan(/[\d]/).join,
+        :ping_data    => ping,
+        :ping_id      => ping.split.grep(/icmp_req/).join.scan(/[\d]/).join,
         :ping_time_ms => ping.split.grep(/time/).join.scan(/[\d\.]/).join,
-        :ping_ttl => ping.split.grep(/ttl/).join.scan(/[\d\.]/).join
+        :ping_ttl     => ping.split.grep(/ttl/).join.scan(/[\d\.]/).join
       }
     end
-    @ping_results = {
-      :packets_transmitted => packets_transmitted,
-      :packets_received => packets_received,
-      :packet_loss => packet_loss,
-      :packet_errors => packet_errors,
-      :completion_time => completion_time,
-      :min => min, 
-      :average => average,
-      :max => max,
-      :host=> host,
-      :time => "#{time}",
-      :rounded_time => Time.at(((time.to_i / 60.0).round * 60.0).to_i),
-      :transmitted_to_database => false,
-      :test_case_id => "#{@test_case_id}"
-    }
 
+    @ping_results = {
+      :packets_transmitted      => packets_transmitted,
+      :packets_received         => packets_received,
+      :packet_loss              => packet_loss,
+      :packet_errors            => packet_errors,
+      :completion_time          => completion_time,
+      :min                      => min, 
+      :average                  => average,
+      :max                      => max,
+      :host                     => host,
+      :time                     => "#{time}",
+      :rounded_time             => Time.at(((time.to_i / 60.0).round * 60.0).to_i),
+      :transmitted_to_database  => false,
+      :test_case_id             => "#{@test_case_id}"
+    }
   end
 
   def results
@@ -158,14 +153,16 @@ class Ping
     @time = Time.now
     @pings << `ping #{@hostname} -c #{@count}` + "-----Time Started #{@time} \n" + "----- Host #{@hostname}" 
   end
+
   def pings
     @pings
   end
+
 end
 
-
-#stores a large array of ping requests
+# stores a large array of ping requests
 class PingData
+
   def initialize
     @all_data = []
     @file_data = ""
@@ -187,12 +184,10 @@ class PingData
   end
 
   def successful_find_by_host(host_name)
-    #@successful_find_by_host ||= successful_pings.select{|a| a.host == host_name}
     successful_pings.select{|a| a.host == host_name}
   end
 
   def failed_find_by_host(host_name)
-    #@failed_find_by_host ||= failed_pings.select{|a| a.host == host_name}
     failed_pings.select{|a| a.host == host_name}
   end
 
@@ -202,8 +197,8 @@ class PingData
 
 
   def sort_by_host(hosts)
-    hosts.group_by {|p| p.host}
     # returns ["google.com", PingParser{:average => "12.00" ....}]
+    hosts.group_by {|p| p.host}
   end
 
   def sort_and_cache(host_pings)
@@ -233,64 +228,14 @@ class PingData
   def save_staging_file
     File.open("#{$pingbox_root}/ping.yml", "w+") {|f| f.write(@all_data.map(&:results).to_yaml) }
   end
+
   def sha_ping_file
     @hash_file_name = Hasher.new("#{$pingbox_root}/ping.yml").hashsum
   end
+
   def zip_ping_file
     system("gzip -9 #{$pingbox_root}/ping.yml")
     FileUtils.mv "#{$pingbox_root}/ping.yml.gz", "#{$pingbox_root}/data/#{@hash_file_name}.gz"
-  end
-  def sent_all_files_to_s3
-    s3_service = S3::Service.new(
-      :access_key_id     => ENV['ec2_access_key_id'],
-      :secret_access_key => ENV['ec2_secret_access_key']
-    )
-    begin
-
-      if Ping.env? == :production
-        @bucket = "pingbox-data"
-      else
-        @bucket = "pingbox-data-dev"
-      end
-
-      bucket = s3_service.buckets.find("#{@bucket}")
-
-
-      # Copy to S3
-      puts "Uploading file to s3"
-      # found_object.destroy
-      Dir.glob("#{$pingbox_root}/data/*.gz") do |file|
-        new_object = bucket.objects.build("#{File.basename(file)}")
-        new_object.content = open("#{file}")
-        new_object.save
-        if new_object.exists?
-          FileUtils.rm(file)
-        end
-      end
-      puts "Sent all data to s3"
-    rescue
-      puts "Error Sending to S3"
-    end
-  end
-
-  def save_file
-    #    save_staging_file
-    #    sha_ping_file
-    #    zip_ping_file
-    #    puts "Saving to S3"
-    #    SendToS3.send_files
-
-    #    sent_all_files_to_s3
-
-
-    #load_file
-    #if @file_data 
-    #@file_data.concat(@all_data.map(&:results))
-    #File.open("#{$pingbox_root}/ping.yml", "w+") {|f| f.write(@file_data.to_yaml) }
-    #else
-    #File.open("#{$pingbox_root}/ping.yml", "w+") {|f| f.write(@all_data.map(&:results).to_yaml) }
-    #end
-
   end
 
   def save(data)
@@ -298,5 +243,4 @@ class PingData
   end
 
 end
-
 
